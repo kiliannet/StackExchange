3fff56e8b4e374dcc74c193fd0ac239076cc0527
Eduardo K. Palenzuela Darias <eduardo.palenzuela@gmail.com>
2016-12-30 10:00:38 +0000

    Change comments.

--- StackExchange/1-Presentation/Application/AppDelegate/AppDelegate.m
+++ StackExchange/1-Presentation/Application/AppDelegate/AppDelegate.m
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.m
-//  IT-eBooks_begin
+//  StackExchange
 //
 //  Created by Eduardo K. Palenzuela Darias on 15/7/15.
 //  Copyright (c) 2015 Eduardo K. Palenzuela Darias. All rights reserved.
